32a566a72412f410bd8df8fa7f0e36e764226e32
vovapasko <vovapasko1699@gmail.com>
2022-09-05 14:52:15 +0200

    feat: changed ports on requesting the books

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -149,7 +149,7 @@ export default {
   },
   methods: {
     getBooks() {
-      const path = 'http://localhost:5000/books';
+      const path = 'http://localhost:8000/books';
       axios.get(path)
         .then((res) => {
           this.books = res.data.books;
@@ -160,7 +160,7 @@ export default {
         });
     },
     addBook(payload) {
-      const path = 'http://localhost:5000/books';
+      const path = 'http://localhost:8000/books';
       axios.post(path, payload)
         .then(() => {
           this.getBooks();
@@ -216,7 +216,7 @@ export default {
       this.updateBook(payload, this.editForm.id);
     },
     updateBook(payload, bookID) {
-      const path = `http://localhost:5000/books/${bookID}`;
+      const path = `http://localhost:8000/books/${bookID}`;
       axios.put(path, payload)
         .then(() => {
           this.getBooks();
@@ -236,7 +236,7 @@ export default {
       this.getBooks(); // why?
     },
     removeBook(bookID) {
-      const path = `http://localhost:5000/books/${bookID}`;
+      const path = `http://localhost:8000/books/${bookID}`;
       axios.delete(path)
         .then(() => {
           this.getBooks();
