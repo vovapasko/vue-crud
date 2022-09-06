e97f8da9426c80774324a1f3d7f59a39058b1ea7
vovapasko <vovapasko1699@gmail.com>
2022-09-06 18:56:54 +0200

    chore: removed Ping component

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Books from '../components/Books.vue';
-import Ping from '../components/Ping.vue';
 
 Vue.use(Router);
 
@@ -13,11 +12,6 @@ export default new Router({
       path: '/',
       name: 'Books',
       component: Books,
-    },
-    {
-      path: '/ping',
-      name: 'Ping',
-      component: Ping,
     },
   ],
 });
